b1386323d1970334dc6b76b100800db04c36bde4
cworld1 <cworld0@qq.com>
2022-12-17 04:37:56 +0800

    fix: :bug: fix data nesting problem(*3)

--- hexo_circle_of_friends/run.py
+++ hexo_circle_of_friends/run.py
@@ -58,7 +58,7 @@ def settings_friends_json_parse(json_file, user_conf):
     elif data_type == 2:
         # 进阶格式
         try:
-            for link in json_file["friends"]:
+            for link in friends:
                 # link_list = dic["link_list"]
                 # for link in link_list:
                 # 必须有name、link、avatar字段
