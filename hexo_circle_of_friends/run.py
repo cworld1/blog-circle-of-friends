ce6c7b952066c267c5014e1f152480ed26839773
cworld1 <cworld0@qq.com>
2022-12-17 20:54:33 +0800

    fix: :bug: fix data problem (*5)

--- hexo_circle_of_friends/run.py
+++ hexo_circle_of_friends/run.py
@@ -58,9 +58,10 @@ def settings_friends_json_parse(json_file, user_conf):
     elif data_type == 2:
         # 进阶格式
         try:
-            for link in friends:
-                # link_list = dic["link_list"]
-                # for link in link_list:
+            # for dic in json_file["friends"]:
+            # link_list = dic["link_list"]
+            link_list = json_file["friends"]
+            for link in link_list:
                 # 必须有name、link、avatar字段
                 logger.warning(f"----一个Link----")
                 logger.warning(link)
@@ -69,10 +70,10 @@ def settings_friends_json_parse(json_file, user_conf):
                 avatar = link.get("avatar")
                 suffix = link.get("suffix")
                 if name and friendlink and avatar:
-                    oneFriend = [name, friendlink, avatar]
+                    friends = [name, friendlink, avatar]
                     if suffix:
-                        oneFriend.append(suffix)
-                    user_conf["SETTINGS_FRIENDS_LINKS"]["list"].append(oneFriend)
+                        friends.append(suffix)
+                    user_conf["SETTINGS_FRIENDS_LINKS"]["list"].append(friends)
         except:
             logger.warning(f"json_api进阶格式解析错误")
     else:
